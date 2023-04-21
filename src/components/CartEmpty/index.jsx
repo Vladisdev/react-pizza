e6bdbfe109a894da9c85142f8c8266c7728b2b11
Владислав Попов <vladislavpopov.dev@gmail.com>
2023-04-22 00:15:59 +0300

    empty cart fix

--- src/components/CartEmpty/index.jsx
+++ src/components/CartEmpty/index.jsx
@@ -4,28 +4,28 @@ import emptyCartImg from '../../assets/img/cart-empty.png';
 import style from './CardEmpty.module.scss';
 
 const CartEmpty = () => {
-	return (
-		<div className='container'>
-			<div className={style.cartEmpty}>
-				<h2>Корзина пустая 😕</h2>
-				<p>
-					Вероятней всего, вы не заказывали ещё пиццу.
-					<br />
-					Для того, чтобы заказать пиццу, перейди на главную страницу.
-				</p>
-				<img
-					src={emptyCartImg}
-					alt='Empty cart'
-				/>
-				<Link
-					to={'/'}
-					className={style.button__black}
-				>
-					Вернуться назад
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className='container'>
+      <div className={style.cartEmpty}>
+        <h2>Корзина пустая 😕</h2>
+        <p>
+          Вероятней всего, вы не заказывали ещё пиццу.
+          <br />
+          Для того, чтобы заказать пиццу, перейди на главную страницу.
+        </p>
+        <img
+          src={emptyCartImg}
+          alt='Empty cart'
+        />
+        <Link
+          to={'/'}
+          className={style.button__black}
+        >
+          Вернуться назад
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CartEmpty;
